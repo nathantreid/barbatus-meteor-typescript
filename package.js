405424500d0cee765fce96692a6b471074233af1
Nathan Reid <nathan-code@nathantreid.com>
2017-05-02 09:37:27 -0500

    version bump

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
  */
 Package.describe({
   name: 'nathantreid:typescript',
-  version: '0.0.1',
+  version: '0.0.2-fixtypings.3',
   summary: 'TypeScript for Meteor',
   git: 'https://github.com/nathantreid/meteor-typescript',
   documentation: 'README.md'
@@ -11,7 +11,7 @@ Package.describe({
 
 Package.registerBuildPlugin({
   name: 'typescript',
-  use: ['nathantreid:typescript-compiler'],
+  use: ['nathantreid:typescript-compiler@0.0.2-fixtypings.0.8.3.6'],
   sources: ['plugin.js']
 });
 
@@ -19,17 +19,17 @@ Package.onUse(function(api) {
   api.versionsFrom('1.4.1');
 
   api.use('isobuild:compiler-plugin@1.0.0');
-  api.use('nathantreid:typescript-compiler@0.0.1');
+  // api.use('nathantreid:typescript-compiler@0.0.2-fixtypings.1');
 
   api.imply('modules@0.7.7');
 
   api.imply('barbatus:typescript-runtime@1.0.0');
 });
-
-Package.onTest(function(api) {
-  api.use('tinytest@1.0.12');
-  api.use('barbatus:typescript');
-
-  api.addFiles('tests/runtime-tests.ts', 'client');
-  api.addFiles('tests/runtime-react-tests.tsx', 'client');
-});
+//
+// Package.onTest(function(api) {
+//   api.use('tinytest@1.0.12');
+//   api.use('barbatus:typescript');
+//
+//   api.addFiles('tests/runtime-tests.ts', 'client');
+//   api.addFiles('tests/runtime-react-tests.tsx', 'client');
+// });
